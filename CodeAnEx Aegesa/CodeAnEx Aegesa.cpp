e2d57d0d8432db02b08718bd2d1642271503c59d
Solomon Pattee <azukastyer@gmail.com>
2020-03-09 06:42:19 -0500

    Add project files.

---
+++ CodeAnEx Aegesa/CodeAnEx Aegesa.cpp
@@ -0,0 +1,142 @@
+// CodeAnEx Aegesa.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Created by: Solomon Pattee
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <list>
+#include <iterator>
+#include "World.h"
+
+using namespace std;
+
+// templates
+template <class printedT>
+printedT output(string i)
+{
+	cout << setprecision(2) << fixed << endl;
+	return i;
+}
+
+// globals
+
+World d;
+
+// function declarations
+void menyu();
+string dialougeInput(string);
+
+int main()
+{
+	system("Color 0A");
+	// formating
+	// title screen
+#pragma region Title
+
+	cout 
+		<< "####################################################################################################################\n"
+		<< "#####################################################################################################################\n"
+		<< "#####################################################################################################################\n"
+		<< "   CCCCCCCCCCC        OOOOOOOOOO            DDDDDDDD              EEEEEEEEEEEEEEEEEEE   \n"
+		<< "  CCCCC              OOO        OOO         DD       DD           EEEEEEEE    \n"
+		<< " CC                 OO           OO         DD         DD          EE     \n"
+		<< "CC                 OO              OO       DD         DD          EE   \n"
+		<< "CC       =====  == OO              OO  ==  DD         DD   ==    EEEEEEEEE    === \n"
+		<< "CC       =====  == OO             OO ==   DD         DD   ==    EEEEEEEEEE   === \n"
+		<< " CC                OO            OO      DD        DD           EE                                       XXXXX    XXXXXXX\n"
+		<< "  CC                OO          OO      DD       DD            EE                                    XXXX      XXXXXX\n"
+		<< "   CCCCC             OO        OO      DD     DDD             EEEEEE                               XXXX    XXXXX\n"
+		<< "    CCCCCCCCCCC       OOOOOOO         DDDDDDDDD              EEEEEEEEEEEEEEEEE                    XXX    XXXX\n"
+		<< "                                                                     XXXXXX     XXXX          XXXXX  XXXXX\n"
+		<< "   AAAAAAA                                     EEEEEEEEEEEEEEEE             XXX     XX      XXX   XXX\n"
+		<< "   AA    AA                                   EEEEE                             XX   XX XXX      XXX\n"
+		<< "  AAA    AAA                                 EEEE                                  X XXXXXXXX\n"
+		<< "  AAAA     AAAA                              EEE                                   XXXXXXX \n"
+		<< "  AA  AAAAAAAAAAA          NN    NNNNNN     EEEEEEEEE    ===   ==    =          XXXXXXXXX             == = == = \n"
+		<< "   AA          AAAA         NNNN       NN   EEEEEEEEEEE   == ===   =         XXXXXXX  XXXXX XX = =   ===  = \n"
+		<< "    AA           AAAAA       NN         NN   EEEE                        XXXXXX        XXXX    XXX   \n"
+		<< "     AA            AAAAA      NN         NN     EEEE                  XXXXXX            XXXX    XXXXXX \n"
+		<< "      AA              AAAAA     NN         NN      EEEEEEEEEE    XXXXXXXX                 XXXX    XXXXX\n"
+		<< "       AA                AAAAA   NN          NN     EEEEEEEEEEEEEEEE                         XXX    XXXXXXXXX\n"
+		<< "                                                                                                      XXXXXXXXXXXXXXXXX\n"
+		<< "################################################################################################################################# = =     - - - -  + +  \n"
+		<< "==========         ------           ------=-=-=-              ===   = -=--           =---==--  ===--            ========- -    --+ == +=+ = -\n"
+		<< "==========  ==    ------    ----------=----          =-=---=     === = --    ----------=----==     = ===----====    =====  ==    -----  \n"
+		<< "=========   ===   ------    -------=---==-      --= ========      -  = --    --=============== ==    ====================   ===   ----+   +    +  + + -  \n"
+		<< "========   ======-------   ---------=------   =  -= =========   = -= -=-=-     --------=------=====     ==== ============   ======----*   *   **    * * --    \n"
+		<< "=======           ------==            ---=-     ==== ===============  = --==          ---=-= ==========      ===========           --- + +   ++ + -+   + ++-    \n"
+		<< "======    ======    --------   -----=------ --      --======-=        ===---==   -----=------=---- === ===       ======    ======    ---            \n"
+		<< "=====    ========    =======    ====-=------ --       = = =====   -  = -======    ====-=----- ===========         ====    ========    == + +   + +     \n"
+		<< "=====    ========     =======               -=-- -                -= -=========               ===              =======    ========     =   +  + +  + +   \n"
+		<< "=================---------------------=--------========================-=- -  = -= -=-=- = ========---- === ========    \n"
+
+		<< endl;
+#pragma endregion
+
+	cout << "Press Enter to Play" << endl;
+	cin.get();
+
+	// gamemenu
+
+	menyu();
+
+	system("PAUSE");
+}
+
+void menyu()
+{
+	system("CLS");
+	int choice = 's';
+	cout << "Main Menu\n"
+		<< "\n"
+		<< "New Game (n)\n"
+		<< "Continue (c)\n"
+		<< "Quit Game (q)" << endl;
+
+	// choice goes here
+	cin >> choice;
+	do
+	{
+		switch (choice)
+		{
+		case 'n':
+		case 1:
+		case 'N':
+
+			d.beginningStory();
+			choice = '|';
+			break;
+
+		case 'c':
+		case 2:
+		case 'C':
+
+			break;
+
+		case 'q':
+		case 3:
+		case 'Q':
+			cout << endl;
+			cout << "GoodBye!" << endl;
+			choice = '|';
+			break;
+
+		default:
+			cout << "Invalid option..." << endl;
+			menyu();
+			break;
+		};
+	} while (choice != '|' || choice != '~');
+}
+
+string dialougeInput(string pvalue)
+{
+	getline(cin, pvalue);
+	pvalue.erase(remove(pvalue.begin(), pvalue.end(), '\n'), pvalue.end());
+	return pvalue;
+}
